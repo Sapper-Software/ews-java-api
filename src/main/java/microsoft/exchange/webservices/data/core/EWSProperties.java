12da9b0ef50efa4ef4134180799320ce850ce148
Subho Ghosh <subho.ghosh@outlook.com>
2020-12-26 12:27:33 +0530

    Sync

--- src/main/java/microsoft/exchange/webservices/data/core/EWSProperties.java
+++ src/main/java/microsoft/exchange/webservices/data/core/EWSProperties.java
@@ -6,7 +6,7 @@ public class EWSProperties extends Properties {
     private static final String PROP_DEF_REPLACE_CHAR = "ews.xml.replace.char";
     private static final String PROP_DEF_REPLACE_REGEX = "ews.xml.replace.regex";
     private static final String DEFAULT_XML_REPLACE_CHAR = "&#63;";
-    private static final String DEFAULT_XML_REPLACE_REGEX = "&#x0*[1-8]; | [^\\u0001-\\uD7FF\\uE000-\\uFFFD\\u10000-\\u10FFFF]";
+    private static final String DEFAULT_XML_REPLACE_REGEX = "&#x0*[1-8];|[^\\u0001-\\uD7FF\\uE000-\\uFFFD\\u10000-\\u10FFFF]";
 
     public EWSProperties() {
         setDefaultXmlReplaceChar(DEFAULT_XML_REPLACE_CHAR);
